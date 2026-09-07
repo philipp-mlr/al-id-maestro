1c6e53e55d3a6efce92e48b405f64d854c5f167a
Philipp Müller <hello@philipp.software>
2026-09-07 20:21:11 +0200

    feat: loop through branches

--- flake.nix
+++ flake.nix
@@ -9,18 +9,15 @@
     };
   };
 
-  outputs =
-    { self, ... }@inputs:
-    let
-      supportedSystems = [
-        "x86_64-linux"
-        "aarch64-linux"
-        "aarch64-darwin"
-      ];
-      forEachSupportedSystem =
-        f:
-        inputs.nixpkgs.lib.genAttrs supportedSystems (
-          system:
+  outputs = {self, ...} @ inputs: let
+    supportedSystems = [
+      "x86_64-linux"
+      "aarch64-linux"
+      "aarch64-darwin"
+    ];
+    forEachSupportedSystem = f:
+      inputs.nixpkgs.lib.genAttrs supportedSystems (
+        system:
           f {
             inherit system;
             pkgs = import inputs.nixpkgs {
@@ -29,51 +26,50 @@
                 inputs.self.overlays.default
               ];
             };
+            lib = inputs.nixpkgs.lib;
           }
-        );
-    in
-    {
-      overlays.default = final: prev: {
-        rustToolchain =
-          with inputs.fenix.packages.${prev.stdenv.hostPlatform.system};
-          combine (
-            with stable;
-            [
-              clippy
-              rustc
-              cargo
-              rustfmt
-              rust-src
-            ]
-          );
-      };
-
-      devShells = forEachSupportedSystem (
-        {
-          pkgs,
-          system,
-        }:
-        {
-          default = pkgs.mkShell {
-            packages = with pkgs; [
-              rustToolchain
-              openssl
-              pkg-config
-              cargo-deny
-              cargo-edit
-              cargo-watch
-              self.formatter.${system}
-            ];
-
-            shellHook = ''
-              if [ -t 0 ]; then
-                exec fish
-              fi
-            '';
-          };
-        }
       );
-
-      formatter = forEachSupportedSystem ({ pkgs, ... }: pkgs.nixfmt);
+  in {
+    overlays.default = final: prev: {
+      rustToolchain = with inputs.fenix.packages.${prev.stdenv.hostPlatform.system};
+        combine (
+          with stable; [
+            clippy
+            rustc
+            cargo
+            rustfmt
+            rust-src
+          ]
+        );
     };
+
+    devShells = forEachSupportedSystem (
+      {
+        pkgs,
+        lib,
+        system,
+      }: {
+        default = pkgs.mkShell {
+          LD_LIBRARY_PATH = lib.makeLibraryPath [pkgs.openssl];
+          packages = with pkgs; [
+            rustToolchain
+            openssl
+            pkg-config
+            cargo-deny
+            cargo-edit
+            cargo-watch
+            self.formatter.${system}
+          ];
+
+          shellHook = ''
+            if [ -t 0 ]; then
+              exec fish
+            fi
+          '';
+        };
+      }
+    );
+
+    formatter = forEachSupportedSystem ({pkgs, ...}: pkgs.nixfmt);
+  };
 }
--- src/utilized_object_provider/git.rs
+++ src/utilized_object_provider/git.rs
@@ -3,7 +3,10 @@ use std::{
     path::Path,
 };
 
-use git2::RepositoryInitOptions;
+use axum::extract::path;
+use git2::{Error, Repository, RepositoryInitOptions};
+use regex::Regex;
+use walkdir::WalkDir;
 
 use crate::{id_state::IDState, object_tree::ObjectTree, object_type::ObjectType};
 
@@ -13,11 +16,17 @@ pub struct GitUtilizedObjectProvider;
 
 impl super::UtilizedObjectProvider for GitUtilizedObjectProvider {
     fn generate_id_tree(&self) -> ObjectTree {
-        let url = "https://github.com/microsoft/BCapps";
+        let url = "https://github.com/fond-of/bc-apps";
+        let path = Path::new("/home/philipp/repos/al-id-maestro/repos");
 
         let mut callbacks = git2::RemoteCallbacks::new();
         callbacks.credentials(|_url, username_from_url, _allowed_types| {
             git2::Cred::userpass_plaintext("philipp-mlr", "")
+        });
+
+        callbacks.update_tips(|tip, oid1, oid2| {
+            println!("Something is updating: {}", tip);
+            true
         });
 
         let mut fo = git2::FetchOptions::new();
@@ -26,9 +35,34 @@ impl super::UtilizedObjectProvider for GitUtilizedObjectProvider {
         let mut builder = git2::build::RepoBuilder::new();
         builder.fetch_options(fo);
 
-        let repo = builder
-            .clone(url, Path::new("/home/philipp/repos/al-id-maestro/repos"))
-            .unwrap();
+        let repo = Self::open_repo(&mut builder, path, url).unwrap();
+
+        let branches = repo.branches(Some(git2::BranchType::Remote)).unwrap();
+
+        for branch_result in branches {
+            let (branch, _btype) = branch_result.unwrap();
+            branch.get().target().unwrap();
+
+            repo.set_head(branch.name().unwrap().unwrap());
+
+            let mut cb = git2::build::CheckoutBuilder::new();
+            repo.checkout_head(Some(cb.force()));
+
+            let re = Regex::new(r#"^(\w+) (\d{1,6}) "?"?([^"]*)?"?"#).unwrap();
+            for entry in WalkDir::new(path) {
+                let entry = entry.unwrap();
+                if !entry.file_type().is_file() {
+                    continue;
+                }
+                let path = entry.path();
+                let text = std::fs::read_to_string(path).unwrap();
+                for m in re.find_iter(&text) {
+                    println!("{}:{}: {:?}", path.display(), m.start(), m.as_str());
+                }
+            }
+
+            println!("checked out to {}", branch.name().unwrap().unwrap());
+        }
 
         let start = 50000u32;
         let end = 99999u32;
@@ -41,5 +75,34 @@ impl super::UtilizedObjectProvider for GitUtilizedObjectProvider {
         let mut map = HashMap::new();
         map.insert(ObjectType::Codeunit, id_range);
         ObjectTree::new(map)
+    }
+}
+
+impl GitUtilizedObjectProvider {
+    fn open_repo(
+        builder: &mut git2::build::RepoBuilder,
+        path: &Path,
+        url: &str,
+    ) -> Result<Repository, git2::Error> {
+        let name = url.strip_prefix("https://").unwrap().replace("/", "-");
+
+        match builder.clone(url, path) {
+            Ok(v) => Ok::<git2::Repository, git2::Error>(v),
+            Err(e) => {
+                if e.code() == git2::ErrorCode::Exists && e.class() == git2::ErrorClass::Invalid {
+                    println!(
+                        "A repository already exists at {}. Opening instead",
+                        path.to_str().unwrap()
+                    );
+                    return Repository::open(path);
+                }
+                return Err(e);
+            }
+        };
+        Err(git2::Error::new(
+            git2::ErrorCode::GenericError,
+            git2::ErrorClass::None,
+            "Unexpected error occurred",
+        ))
     }
 }
